3f10830fb06f0142fd3a5aff575c14fb0b514cd2
Thomas Domingues <thomasdomingues@icloud.com>
2020-12-15 12:29:39 +0100

    7. Comment behaviour of current items

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -14,22 +14,28 @@ class GildedRose {
         for (Item item : items) {
             if (!item.name.equals("Aged Brie")
                     && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                // Normal item
                 if (item.quality > MINIMUM_QUALITY) {
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        // Normal item
                         decreaseQuality(item);
                     }
+                    // Implicit else: Legendary item
                 }
             } else {
+                // Aged Brie or Backstage pass
                 if (isUnderHighestQualityValue(item)) {
                     increaseQuality(item);
 
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        // Only Backstage pass, increase by 2 when is less than 10 days before concert
                         if (item.sellIn < 11) {
                             if (isUnderHighestQualityValue(item)) {
                                 increaseQuality(item);
                             }
                         }
 
+                        // Only Backstage pass, increase by 3 when is less than 5 days before concert
                         if (item.sellIn < 6) {
                             if (isUnderHighestQualityValue(item)) {
                                 increaseQuality(item);
@@ -39,6 +45,7 @@ class GildedRose {
                 }
             }
 
+            // Normal item
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 item.sellIn = item.sellIn - 1;
             }
@@ -47,14 +54,18 @@ class GildedRose {
                 if (!item.name.equals("Aged Brie")) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.quality > MINIMUM_QUALITY) {
+                            // Normal item except legendary
                             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                                 decreaseQuality(item);
                             }
+                            // Implicit else: Legendary item
                         }
                     } else {
+                        // Backstage pass only
                         item.quality = MINIMUM_QUALITY;
                     }
                 } else {
+                    // Aged Brie only
                     if (isUnderHighestQualityValue(item)) {
                         increaseQuality(item);
                     }
